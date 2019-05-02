a3f168d08a270c7f3609d5ae417dcfd58f5bff20
Mohamed Nasr Mohamed Ali <mohamedn6666@gmail.com>
2019-05-02 23:18:42 +0200

    build the mini project

--- mindstroms.py
+++ mindstroms.py
@@ -1,28 +1,34 @@
 import turtle
 
 
-def draw_square():
+def draw_square(some_turtle):
     # move forward and turn right makes these steps four times
+    for i in range(4):
+        some_turtle.forward(100)
+        some_turtle.right(90)
     
+
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor('red')
+    #Create the turtle Brad - Draws a square
     brad = turtle.Turtle()
     brad.shape('turtle')
     brad.color('yellow','green')
-    brad.speed(2)
-    for i in range(4):
-        brad.forward(100)
-        brad.right(90)
+    brad.speed(0)
+    for i in range(36):
+      draw_square(brad)
+      brad.right(10)
 
-def draw_circle():
-    # draw circle with turtle library
+    '''#Create the turtle Angie - Draws a circle
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
-    
+    '''
+
+    window.exitonclick()
 
 
-window = turtle.Screen()
-window.bgcolor("red")
-draw_square()
-draw_circle()
-window.exitonclick()
+draw_art()
