d9fa9564aa65a5bac06ff9d100afe5d5eebd423d
Mohamed Nasr Mohamed Ali <mohamedn6666@gmail.com>
2019-05-02 22:53:32 +0200

    Improved Code Quality

--- mindstroms.py
+++ mindstroms.py
@@ -3,20 +3,26 @@ import turtle
 
 def draw_square():
     # move forward and turn right makes these steps four times
-    window = turtle.Screen()
-    window.bgcolor("red")
-
-
+    
     brad = turtle.Turtle()
     brad.shape('turtle')
     brad.color('yellow','green')
     brad.speed(2)
-
     for i in range(4):
         brad.forward(100)
         brad.right(90)
 
+def draw_circle():
+    # draw circle with turtle library
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+    
 
-    window.exitonclick()
 
+window = turtle.Screen()
+window.bgcolor("red")
 draw_square()
+draw_circle()
+window.exitonclick()
